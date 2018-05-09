1150ea1c739d57b5aab63e9b21e32fe1cf62a2bb
Adam Wieckert <wiec1369@gmail.com>
2018-05-09 12:06:48 -0500

    Made it so that the submit button is disabled once a valid ammount has been set

--- javascripts/disabled.js
+++ javascripts/disabled.js
@@ -4,10 +4,12 @@ console.log('alltheBoxes: ', allTheBoxes);
 
 const enableButtons = () => {
   const inputValue = document.getElementById('project-budget').innerHTML;
+  const submit = document.getElementById('submit-budget');
   if (inputValue !== '') {
     for (let i = 0; i < allTheBoxes.length; i++) {
       allTheBoxes[i].removeAttribute('disabled', '');
     }
+    submit.setAttribute('disabled', '');
   }
 };
 
