4cc8d8eda767b676508162f9fe95b08e86ae33eb
Adam Wieckert <wiec1369@gmail.com>
2018-05-09 20:01:39 -0500

    Added the Screen Shots

--- javascripts/secondPrintToDom.js
+++ javascripts/secondPrintToDom.js
@@ -9,11 +9,10 @@ const printItems = (selectedItem) => {
 const itemToDom = (itemToPrint) => {
   let stringToPrint = '';
   stringToPrint += `<div id="${itemToPrint[0].categoryId}" class="col-md-6">`;
-  stringToPrint += `<h3>${itemToPrint[0].name}: $${itemToPrint[0].cost}</h3>`;
+  stringToPrint += `<h3 class='white'>${itemToPrint[0].name}: $${itemToPrint[0].cost}</h3>`;
   stringToPrint += `</div>`;
   const remainingBudget = (data.getProjectBudget() - data.getCurrentBudget()).toString();
   currentBudgeth2.innerHTML = '$' + remainingBudget;
-  console.log('currentbudgeth2: ', currentBudgeth2);
   return stringToPrint;
 };
 
